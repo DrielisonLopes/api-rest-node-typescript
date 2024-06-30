57ade17f68aa9e28a59c8a01361b0f1056c7395c
DrielisonLopes <drielisonl@gmail.com>
2024-06-29 22:53:16 -0300

    feat: Multiple error validation

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -1,26 +1,35 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup'; 
 
 interface ICities {
     name: string;
+    state: string;
 }
 
-const bodyValidation: yup.Schema<ICities> = yup.object().shape({
+const bodyValidation: yup.ObjectSchema<ICities> = yup.object().shape({
     name: yup.string().required().min(3),
-});	
+    state: yup.string().required().min(3),
+  });
 
-export const create = async (req: Request<{}, {}, ICities>, res: Response) => {
-    let validateData: ICities | undefined = undefined;
+  export const create = async (req: Request<{}, {}, ICities>, res: Response) => {
+    let validatedData: ICities | undefined = undefined;
 
     try {
-        validateData = await bodyValidation.validate(req.body);
-    } catch (error) {
-        const yupError = error as yup.ValidationError;
-
-        return res.status(400).json({ 
-            erros: {
-                default: yupError.message, 
-            }
+        validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+      } catch (err) {
+        const yupError = err as yup.ValidationError;
+        const errors: Record<string, string> = {};
+    
+        yupError.inner.forEach(error => {
+          if (error.path === undefined) return;
+          errors[error.path] = error.message;
         });
-    }
-};
+    
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+      }
+    
+      console.log(validatedData);
+    
+      return res.send('Create!');
+    };
