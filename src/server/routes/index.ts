a0729e68c777d62f7edfb5ff28355138f4b5a89a
DrielisonLopes <drielisonl@gmail.com>
2024-10-09 20:24:17 -0300

    feat: update background color

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,8 +4,24 @@ import { CitiesController } from './../controllers';
 const router = Router();
 
 router.get('/', (_, res) => {
-
-  return res.send("Hello, Node!!");
+  const html = `
+    <html>
+      <head>
+        <style>
+          body {
+            background-color: #333;
+            color: #84c00b;
+            font-family: Arial, sans-serif;
+            padding: 16px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hello, Node!!</h1>
+      </body>
+    </html>
+  `;
+  return res.send(html);
 });
 
 router.get('/cities', CitiesController.getAllValidation, CitiesController.getAll);
